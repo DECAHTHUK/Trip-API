76bb1f9a55e579f60fe60e9cc78bb4b6103fe779
Alexei Talanov <alexei.talanov003@mail.ru>
2023-01-29 13:25:58 +0300

    Initial commit

---
+++ src/main/java/tinkoff/lab/tripAPI/TripApiApplication.java
@@ -0,0 +1,13 @@
+package tinkoff.lab.tripAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripApiApplication.class, args);
+	}
+
+}
