4984fcb239006bc6438618b2771ab544536534ca
Alexei Talanov <alexei.talanov003@mail.ru>
2023-02-05 14:30:19 +0300

    Finished refactoring

---
+++ src/test/java/ru/tinkoff/lab/tripAPI/TripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.lab.tripAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
